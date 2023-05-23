a0b4d7ed9996deb95e7e4d32bfe4625d64536f6e
Abdalla Abdiwahab Hussein <abdallaabdiwahab66@gmail.com>
2023-05-23 15:33:47 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import DisplayList from "./DisplayList"
+import UpdateStutus from "./UpdateStutus"
+
+
+
+
+
+function App() {
+  return (
+    <div>
+      <DisplayList/>
+      <UpdateStutus/>
+    </div>
+  );
+}
+
+export default App;
